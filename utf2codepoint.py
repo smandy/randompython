c666ec21ad25b28ad8ae262bfd6cc35c91e0f042
Andy Smith <andyrsmith@gmail.com>
2024-02-19 21:06:11 +0000

    Unicode fun.

---
+++ utf2codepoint.py
@@ -0,0 +1,50 @@
+def utf8_to_code_point(byte_sequence):
+    # Determine the number of bytes used for the character
+    num_bytes = len(byte_sequence)
+    
+    # Determine the bitmask for extracting the bits representing the character
+    masks = [0x7F, 0x1F, 0x0F, 0x07]
+    bitmask = masks[num_bytes - 1]
+    
+    # Extract the bits representing the character from the byte sequence
+    code_point = byte_sequence[0] & bitmask
+    for byte in byte_sequence[1:]:
+        code_point = (code_point << 6) | (byte & 0x3F)
+    
+    return code_point
+
+
+def code_point_to_utf8(code_point):
+    if code_point < 0x80:
+        return bytes([code_point])
+    elif code_point < 0x800:
+        byte1 = 0xC0 | (code_point >> 6)
+        byte2 = 0x80 | (code_point & 0x3F)
+        return bytes([byte1, byte2])
+    elif code_point < 0x10000:
+        byte1 = 0xE0 | (code_point >> 12)
+        byte2 = 0x80 | ((code_point >> 6) & 0x3F)
+        byte3 = 0x80 | (code_point & 0x3F)
+        return bytes([byte1, byte2, byte3])
+    elif code_point < 0x110000:
+        byte1 = 0xF0 | (code_point >> 18)
+        byte2 = 0x80 | ((code_point >> 12) & 0x3F)
+        byte3 = 0x80 | ((code_point >> 6) & 0x3F)
+        byte4 = 0x80 | (code_point & 0x3F)
+        return bytes([byte1, byte2, byte3, byte4])
+    else:
+        raise ValueError("Invalid code point")
+
+# Example usage:
+code_point = 0x65E5  # Unicode code point for character 日
+byte_sequence = code_point_to_utf8(code_point)
+print("UTF-8 byte sequence:", byte_sequence)
+
+
+# Example usage:
+byte_sequence = b'\xE6\x97\xA5'  # UTF-8 byte sequence representing the character 日
+byte_sequence = b'\xEF\xBB\xBF'  # UTF-8 BOM
+code_point = utf8_to_code_point(byte_sequence)
+print("Code point:", hex(code_point))
+
+print("Reverse decode", code_point_to_utf8(code_point))

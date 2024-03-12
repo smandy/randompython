789b598f2bf1449d8e72246f270a9881814c3773
Andy Smith <andyrsmith@gmail.com>
2024-03-12 10:41:51 +0000

    Refine emacs timestamps.

--- emacs_timestamps.py
+++ emacs_timestamps.py
@@ -2,19 +2,22 @@ import datetime
 
 BITS = 64
 EMACS_LSBITS = 16
+MICRO = 1e-6
+PICO = 1e-12
 
 LSB_MASK = (1 << EMACS_LSBITS) - 1
 MSB_MASK = ~LSB_MASK & ((1 << BITS) -1) # Strip off two's complement
 
-# Captured values in the wild - ignore picos and nanos for now.
 # (current-time) (26095 53160 665501 998000)
 ms_bits = 26095
 ls_bits = 53160
+µ = 665501
+pico = 998000
 
 #(26095 << 16) | 53160 & ((1 << 16) - 1 )
 #In [93]: (26095 << 16) | 53160 & ((1 << 16) - 1 )
 # Out[93]: 1710215080
-x = ((ms_bits << EMACS_LSBITS ) & MSB_MASK) | (ls_bits & LSB_MASK)
+x = (((ms_bits << EMACS_LSBITS ) & MSB_MASK) | (ls_bits & LSB_MASK) ) + (µ * MICRO) + (pico * PICO)
 print( x)
 print(datetime.datetime.utcfromtimestamp(x))
 

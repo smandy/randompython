80dcf26cf77b16a81a114cc2927bc0442faf44e1
Andy Smith <andyrsmith@gmail.com>
2024-04-20 09:24:27 +0100

    Polarisation.

--- polarization.py
+++ polarization.py
@@ -6,31 +6,35 @@ from math import pi
 import cmath
 
 twopi = 2 * pi
-
 twopij = complex( 0, twopi)
-
 degs = linspace( 0, twopi, 200, endpoint = True)
 
 clf()
+#title( "Polarisation! Bitches!!!")
 
 for i in range(9):
     subplot( 3,3, i+1)
     grid(True)
     
     phi = i * pi / 4.0
-    phi = i * pi / 16.0
 
+    div = 8
+    phi = i * pi / div
+
+    gfs = pi / 2
+    gfs = 0
+    
     xs = sin(degs + phi)
     ys = sin(degs)
-    title(f"{i} π / 4")
+    title(f"{i} π / {div}")
 
     offset = 10
 
     lw = 8
     
-    plot( xs[:66-offset], ys[:66-offset], 'b-', lw=lw)
-    plot( xs[66:133-offset], ys[66:133-offset], 'r-', lw = lw)
-    plot( xs[133:-offset], ys[133:-offset], 'm-', lw = lw)
+    plot( xs[:66-offset], ys[:66-offset], 'r-', lw=lw)
+    plot( xs[66:133-offset], ys[66:133-offset], 'g-', lw = lw)
+    plot( xs[133:-offset], ys[133:-offset], 'b-', lw = lw)
 
 
 
